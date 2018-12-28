f9810e9574f1de5baf9880b8d4caeef6ccdfa7f7
Javier Martinez Lizama <javier.mlizama@gmx.com>
2018-12-28 11:05:18 +0100

    explicit urls are nowhandled by url_for

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
@@ -18,7 +18,12 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    # will return true when user clicks on submit and all fields are validated ok
+    if form.validate_on_submit():
+        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+        # name of the view function
+        return redirect(url_for('index'))
     return render_template('login.html', title="Sign In", form=form)
